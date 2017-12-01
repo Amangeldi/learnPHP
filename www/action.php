cdcf19bbdc75ed3761e89137bff0b0b5d32a2669
Amangeldi <Amangeldi.9606@gmail.com>
2017-12-01 20:23:26 +0500

    Проверка логина. Как нибудь нужно реализовать тоже самое используя меньше if, по правилам

--- www/action.php
+++ www/action.php
@@ -1,10 +1,26 @@
 ﻿<?php
-$a = $_POST['a']; 
-if($a<0) {
-	echo "Минус<br>";
-} elseif ($a>0) {
-	echo "Плюс<br>";
-} else {
-	echo "Ноль<br>";
+$role = $_POST['role']; 
+$button = $_GET['button'];
+$password = $_POST['password'];
+$STR = <<<ABC
+<form action="action.php" method="post">
+ <p>Пароль? <input type="text" name="password" /></p>
+ <p><input type="submit" value="Войти" /></p>
+</form>
+<form><input type="button" value="Отмена" onClick='location.href="http://php1.ru/action.php?button=cancel"'></form>
+ABC;
+if($button=="cancel") {
+	echo "Вход отменен<br>";
+} elseif($role == "Админ") {
+	echo $STR;
+} elseif($role != "Админ" AND $role >0) {
+	echo "Я вас не знаю<br>";
+	echo $_GET['rty'];
+	$flag = 1;
+}
+if($password=="Черный Властелин"){
+		echo "Добро пожаловать!";
+} elseif($button!="cancel" AND $role != "Админ" AND $flag==0) {
+	echo "Пароль неверен<br>";
 }
 ?> 
