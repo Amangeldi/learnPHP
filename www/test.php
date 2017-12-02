b1431d03dc89059b84018690f4978b994a493f68
Amangeldi <Amangeldi.9606@gmail.com>
2017-12-02 13:34:43 +0500

    while

--- www/test.php
+++ www/test.php
@@ -1,6 +1,7 @@
 <?php
 $a = 2;
 $x = 1 + ($a*=2);
+$a = 0;
 echo "Результат = ".$x."<br>";
 //------
 if("0") {
@@ -18,4 +19,9 @@ echo (null OR 2 OR undefined) . "<br>";
 echo (1 AND null AND 2 ) . "<br>";
 //---
 echo ( null OR 2 AND 3 OR 4 ) . "<br>";
+//-----
+while($a<3) {
+	echo "Номер $a!<br>";
+	$a++;
+}
 ?>
