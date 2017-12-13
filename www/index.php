7e9571b83ed02607d6275df34bbeb7144aac8f60
Amangeldi <Amangeldi.9606@gmail.com>
2017-12-13 21:36:53 +0500

    пример использования php

--- www/index.php
+++ www/index.php
@@ -2,4 +2,20 @@
 $goods = array("Компуктер","Ноут","Планшет");
 $index = count($goods)-1;
 echo $goods[$index];
+$link = mysql_connect("localhost", "root")
+        or die("Не могу соединиться");
+    print ("Соединение прошло успешно");
+	mysql_select_db('testphp', $link);
+	$query = "INSERT INTO lessons (title, body) VALUES (
+'Выражаемся по-ПиЭйчПовски',
+'Итак, на прошлом уроке я обещал начать урок 3 с изучения следующего, четвертого типа данных...'
+)";
+mysql_query($query, $link);
+$query = "SELECT title, body FROM lessons";
+$result = mysql_query($query, $link);
+while ($row = mysql_fetch_array($result)) {
+    echo "<h1>".$row['title']."</h1><p>";
+    echo $row['body']."</p>";
+}
+    mysql_close($link);
 ?>
